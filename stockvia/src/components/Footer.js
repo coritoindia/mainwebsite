7fb0cbfc0b0b844ffc829c26f4a741255b68fe03
coritoindia <coritoindia@gmail.com>
2021-09-15 04:31:07 +0530

    stockvia done

---
+++ stockvia/src/components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer(){
+
+    return (
+        <div className="jumbotron text-center" >
+        <p>Copyright © 2021 Stockvia</p>
+      </div>
+    )
+}
---
+++ stockvia/src/components/Main.js
@@ -0,0 +1,86 @@
+import React,{useState, useEffect} from "react";
+import stockimg from '../assets/stockimage.svg'
+import axios from "axios";
+export default function Main(){
+
+    const [mainData, setMainData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://coritoindia.github.io/stockvia/sample1.json`)
+        .then(res=>{
+            setMainData(res.data)
+            })
+    }, [])
+
+    console.log(mainData)
+
+    return (
+        <div className="container my-5">
+          <h3>All in One Stop for </h3>
+          <p> Stock Market | Mutual Funds | ETF's | Crypto | Digi Gold </p>
+
+          
+            <div className="hero-block">
+
+            <div class="d-flex main-hero p-3">  
+            <div class="p-2 hero-image">
+              <div className="img-block">
+              <img src={stockimg}  width='70%' class="img-fluid" />
+              </div>
+          
+            <h4 className="pt-1">We help your Money grow.</h4>
+          <p> Get Daily Stock Market Update | Stock Recommendations</p>
+
+            </div>
+
+            <div class="p-2 hero-content ">
+
+            <div class="row hero-row align-items-center justify-content-center  ">
+                <div class="col-sm-5 shadow hero-col align-middle dividend-col" >Dividend</div>
+                <div class="col-sm-5 shadow hero-col bonus-col"> Bonus/Split</div>
+                <div class="col-sm-5 shadow hero-col  buy-col"> Buyback/OFS</div>
+                <div class="col-sm-5 shadow hero-col ipo-col"> IPO's</div>
+            </div>
+         
+
+              
+            </div>
+          
+  </div>
+
+           
+{/* 
+                <table class="table">
+                            <thead>
+                              <tr>
+                                <th>Firstname</th>
+                                <th>Lastname</th>
+                                
+                              </tr>
+                            </thead>
+                            {mainData.map (i =>{
+                                return (
+                                
+                                <tbody>
+                                  <tr>
+                                    <td>{i.firstName}</td>
+                                    <td>{i.lastName}</td>
+                                    
+                                  </tr>
+                                  </tbody>
+                                  
+                                )
+                                
+                            })}
+                </table> */}
+
+
+            </div>
+    
+    
+
+        </div>
+ 
+
+    )
+}
